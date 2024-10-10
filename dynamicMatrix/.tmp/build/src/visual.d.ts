9cce96ef10ba2438ecb5f34e7996e092d68ecde3
oliciep <olivercieplinski@gmail.com>
2024-10-10 17:02:33 +0100

    Minor changes and fixes to grid

--- dynamicMatrix/.tmp/build/src/visual.d.ts
+++ dynamicMatrix/.tmp/build/src/visual.d.ts
@@ -1,13 +1,11 @@
-import "./../style/visual.less";
 import powerbi from "powerbi-visuals-api";
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import IVisual = powerbi.extensibility.visual.IVisual;
 export declare class Visual implements IVisual {
-    private host;
+    private target;
     private table;
-    private tableHeader;
-    private tableBody;
     constructor(options: VisualConstructorOptions);
     update(options: VisualUpdateOptions): void;
+    private createRows;
 }
