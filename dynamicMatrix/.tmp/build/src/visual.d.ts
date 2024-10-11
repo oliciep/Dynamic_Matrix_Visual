a6bee3eb1a29aeba0420e63c6bb239290c11c6c2
oliciep <olivercieplinski@gmail.com>
2024-10-11 14:19:29 +0100

    Updated grid structure, added initial hierarchy

--- dynamicMatrix/.tmp/build/src/visual.d.ts
+++ dynamicMatrix/.tmp/build/src/visual.d.ts
@@ -1,11 +1,13 @@
+import "./../style/visual.less";
 import powerbi from "powerbi-visuals-api";
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import IVisual = powerbi.extensibility.visual.IVisual;
 export declare class Visual implements IVisual {
-    private target;
+    private host;
     private table;
+    private tableHeader;
+    private tableBody;
     constructor(options: VisualConstructorOptions);
     update(options: VisualUpdateOptions): void;
-    private createRows;
 }
